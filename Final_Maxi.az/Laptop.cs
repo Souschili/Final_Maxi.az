df3a3090376fc03229280c4d96d27459a60efe0c
Sous Chili <greatdragone75@gmail.com>
2019-01-10 20:09:43 +0400

    Add project files.

---
+++ Final_Maxi.az/Laptop.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Maxi.az
+{
+    public class Laptop: INotifyPropertyChanged
+    {
+        //по умолчанию нонэйм и общая пикча 
+        private string _image { get; set; } = "image/nout.jpg";
+        public string _Image { get { return _image; } set { _image = value; OnPropChanged(); } } 
+        private string _name { get ; set; } = "Noname";
+        public string _Name { get { return _name; } set { _name = value; OnPropChanged(); } }
+        //операционая система
+        private string _os { get; set; } = "Unknown";
+        public string _Os { get {  return String.Format("Operation System {0}", _os); } set { this._os = value; OnPropChanged(); } }
+        //ОЗУ
+        private int _ram { get; set; } = 8;
+        public string _Ram { get
+                                 { return String.Format("RAM is {0} Gb ", _ram ); }
+                             set {
+                                  if (Int32.TryParse(_Ram, out int a)) _ram = a; OnPropChanged();
+            }
+                             }
+
+        //HDD
+        private int _hdd { get; set; } = 500;
+        public string _Hdd
+        {
+            get
+            { return String.Format("HDD capasity is {0} Gb ", _hdd); }
+            set
+            {
+                if (Int32.TryParse(_Hdd, out int a)) _hdd = a; OnPropChanged();
+            }
+        }
+
+        //Monitor size in inch(взял инты для простоты по хорошему децимал надо бы)
+        private int _monitorSize { get; set; } = 21;
+        public string _MonitorSize
+        {
+            get
+            { return String.Format("Monitor size {0} inch ", _monitorSize); }
+            set
+            {
+                if (Int32.TryParse(_MonitorSize, out int a)) _monitorSize = a; OnPropChanged();
+            }
+        }
+
+        //веб камера да\нет по умолчанию да
+        private string _web { get; set; } = "Yes";
+        public string _Web { get { return String.Format("WebCamera {0}", _web); }
+                             set { _web = value; OnPropChanged(); } }
+
+
+        //старая цена
+        private double _oldPrice { get; set; } = 100.0d;
+        public string _OldPrice { get { return String.Format("{0:c}", _oldPrice); }
+                                  set { if (Double.TryParse(_Hdd, out double a)) _oldPrice = a; OnPropChanged(); } }
+
+        //новая цена
+        private double _newPrice { get; set; } = 200.0d;
+        public string _NewPrice { get { return String.Format("{0:c}", _newPrice); }
+                                  set { if (Double.TryParse(_Hdd, out double a)) _newPrice = a; OnPropChanged(); } }
+        public Laptop() { }
+        
+        public Laptop(string image, string name,string os="DOS")
+        {
+            this._image = image;
+            this._name = name;
+            this._os = os;
+        }
+
+        public Laptop(string image, string name, string os, int ram, int hdd,
+                      int monitorSize, string web, double oldPrice, double newPrice) : this(image, name, os)
+        {
+            _ram = ram;
+            _hdd = hdd;
+            _monitorSize = monitorSize;
+            _web = web;
+            _oldPrice = oldPrice;
+            _newPrice = newPrice;
+        }
+
+
+
+
+        //оповещатель укороченый через ? проверка на нулл
+        void OnPropChanged([CallerMemberName]string name = "")
+        {
+            PropertyChanged?.Invoke(this,
+            new PropertyChangedEventArgs(name));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+
+}
+
---
+++ Final_Maxi.az/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+//
+//1. класс Лаптопов в отдельном файле
+// 2. шаблон данных как статик ресурс(из неудачного проекта новый рисовать не хочу)
+// 3. прикручен к листбоксу шаблон,и поставшик данных mycollection
+// 4. Прикрутил 3 модели ноута Acer Extensa EX2508-C5W6,HP 250 G6 (1XP19ES) Dark Ash, MSI GP62M 7RDX(WOT Edition) 
+// 5. Допилить класс цены и тд.
+// 6. Класс допилен с тремя конструкторами ,пустой,урезаный,полный + начения по умолчанию (это для упрощения тестов)
+// TODO Прикрутить интерфейс оповещатель 
+// 7. Все прикрутил и добавил звездочки.
+namespace Final_Maxi.az
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        // тестовый вариант с проверкой вывода рисунка 
+        //public Laptop test { get; set; } = new Laptop();
+
+
+        
+       public ObservableCollection<Laptop> mycollection { get; set; } = new ObservableCollection<Laptop>();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            //данные получаем из класса Лаптоп
+            //this.DataContext = this;// или  new Laptop();
+
+            mycollection.Add(new Laptop("image/Acer Extensa EX2508-C5W6 .jpg", "Acer Extensa EX2508-C5W6"));
+            mycollection.Add(new Laptop("image/HP 250 G6 (1XP19ES) Dark Ash.jpg", "HP 250 G6 (1XP19ES) Dark Ash"));
+
+            //проверка на фулл конструктор ,все ок. но выше для упрощения использовал урезанный конструктор(см. класс)
+            mycollection.Add(new Laptop("image/MSI _WOT.jpg", "MSI GP62M 7RDX(WOT Edition)","Windows 8.1",16,850,21,"no",365.85,325.99));
+
+            // тут только значения по умолчанию
+            mycollection.Add(new Laptop());
+
+
+            //сишарп вариант ,но можно было и в листе itemsource="{Binding Path=mycollection}" 
+            //но тогда контекст меняем this.DataContext = this;
+            //так же можно обойтись вообще без дата контекста достаточно иметь коллекцию обьектов
+            //(отключение датаконтекста показало правоту этого утверждения)
+            NoutView.ItemsSource = mycollection;
+        }
+
+    }
+}
